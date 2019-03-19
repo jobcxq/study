4fc00e0346c50f6529c09578453356f5ec89d8a3
jobcxq <42314252+jobcxq@users.noreply.github.com>
2019-03-19 16:43:27 +0800

    Update SeriableSingletonTest.java

--- src/main/java/com/cxq/designPattern/singleton/seriable/SeriableSingletonTest.java
+++ src/main/java/com/cxq/designPattern/singleton/seriable/SeriableSingletonTest.java
@@ -5,9 +5,6 @@ import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
-/**
- * Created by Tom on 2018/3/8.
- */
 public class SeriableSingletonTest {
     public static void main(String[] args) {
 
