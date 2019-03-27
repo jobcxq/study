2adc0c9badf88148b658a1cee3de4683a74206c6
jobcxq <jobcxq@163.com>
2019-03-27 22:56:18 +0800

    装饰器模式

--- src/main/java/com/cxq/designPattern/decorator/DattercakeTest.java
+++
@@ -1,9 +0,0 @@
-package com.cxq.designPattern.decorator.battercake;
-
-/**
- * @author cnxqin
- * @desc
- * @date 2019/03/27 22:07
- */
-public class DattercakeTest {
-}
---
+++ src/main/java/com/cxq/designPattern/decorator/DecoratorTest.java
@@ -0,0 +1,9 @@
+package com.cxq.designPattern.decorator;
+
+/**
+ * @author cnxqin
+ * @desc 装饰器模式
+ * @date 2019/03/27 22:07
+ */
+public class DecoratorTest {
+}
