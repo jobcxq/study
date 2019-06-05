455f8f580266a0f18abda388fdb088aef8a685d2
jobcxq <jobcxq@163.com>
2019-06-05 21:47:59 +0800

    'thread'

---
+++ src/main/java/com/cxq/javaSE/baseGrammar/TestInteger.java
@@ -0,0 +1,9 @@
+package com.cxq.javaSE.baseGrammar;
+
+/**
+ * @author cnxqin
+ * @desc
+ * @date 2019/06/05 20:08
+ */
+public class TestInteger {
+}
---
+++ src/main/java/com/cxq/javaSE/multThread/SynchronizedDemo.java
@@ -0,0 +1,9 @@
+package com.cxq.javaSE.multThread;
+
+/**
+ * @author cnxqin
+ * @desc
+ * @date 2019/06/05 19:51
+ */
+public class SynchronizedDemo {
+}
