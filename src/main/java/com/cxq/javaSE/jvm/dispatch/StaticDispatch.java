3c3cc7689f6de730be911fde5df2e86401fa98cf
jobcxq <jobcxq@163.com>
2019-09-05 09:42:48 +0800

    静态分派

--- src/main/java/com/cxq/javaSE/jvm/dispatch/StaticDispatch.java
+++ src/main/java/com/cxq/javaSE/jvm/dispatch/StaticDispatch.java
@@ -23,9 +23,10 @@ public class StaticDispatch {
         StaticDispatch dispatch = new StaticDispatch();
         Human man = new Man();      //Human 为变量的静态类型，Man 为实际类型
         Human woman = new Woman();
-        //静态分派，虚拟机重载时通过参数的静态类型来作为判定依据（编译器在编译阶段就可以确定）
+        //虚拟机编译器通过参数的静态类型来作为判定依据，在编译阶段根据参数的静态类型来决定使用的重载版本
+        //静态分派：发生在编译阶段，依赖静态类型来定位方法执行版本的分派，典型应用：方法重载
         dispatch.sayHello(man);     //hello human
-        dispatch.sayHello(woman);   //hello human
+        dispatch.sayHello(woman);
 
     }
 
